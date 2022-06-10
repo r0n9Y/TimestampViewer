a6780da530a371680655d0bf8829cff6f5bf6a36
GitHub Action <action@github.com>
2022-06-10 05:51:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/r0n9y/timestampviewer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/r0n9y/timestampviewer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.r0n9y.timestampviewer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.r0n9y.timestampviewer.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/r0n9y/timestampviewer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/r0n9y/timestampviewer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.r0n9y.timestampviewer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.r0n9y.timestampviewer.MyBundle
 
 class MyProjectService(project: Project) {
 
